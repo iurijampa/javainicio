f0c32d491fe48df29a0c2ab23fbcece6ec8597d1
Iuri Medeiros <xutur12@gmail.com>
2023-08-15 19:40:39 -0300

    Update FolhaPagamento.java
    Adicionei a opção de inserir o valor

--- FolhaPagamento.java
+++ FolhaPagamento.java
@@ -1,22 +1,26 @@
-public class FolhaPagamento {
+import java.util.Scanner;
+public class Main {
     public static void main(String[] args) {
-        double salario1 = 2500.00;
+        Scanner scan = new Scanner(System.in);
+        double salario1;
         double desc = 0;
 
+        System.out.print("Digite o salario:");
+        salario1 = scan.nextDouble();
         if (salario1<=900.00) {
             System.out.println("Isento de desconto");
         }else if (salario1 > 901 && salario1 <= 1500) {
             desc = (5.0 / 100) * salario1;
             System.out.println("Você recebe de 5%");
-            System.out.println(desc);
+            System.out.println("O valor descontado foi de R$" + desc);
         }else if (salario1 > 1500 && salario1 <= 2500) {
             desc = (10.0 / 100) * salario1;
             System.out.println("Desconto de 10%");
-            System.out.println(desc);
+            System.out.println("O valor descontado foi de R$" + desc);
         }else {
             desc = (20.0 / 100) * salario1;
             System.out.println("Desconto de 20%");
-            System.out.println(desc);
+            System.out.println("O valor descontado foi de R$" + desc);
         }
     }
 }
