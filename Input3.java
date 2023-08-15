2905093a1219b197752b2948f785a9711a87f1f2
Iuri Medeiros <xutur12@gmail.com>
2023-08-15 19:47:28 -0300

    Update Input3.java
    Adicionei a opção de colocar as notas

--- Input3.java
+++ Input3.java
@@ -1,18 +1,25 @@
-public class Input3 {
+import java.util.Scanner;
+public class Main2 {
     public static void main(String[] args) {
-        int nota1 = 9;
-        int nota2 = 10;
-        int nota3 = 6;
+        Scanner scan = new Scanner(System.in);
+        double nota1;
+        double nota2;
+        double nota3;
         double total;
         double dividir;
-
+        System.out.print("Digite a primeira nota:");
+        nota1 = scan.nextDouble();
+        System.out.print("Digite a segunda nota:");
+        nota2 = scan.nextDouble();
+        System.out.print("Digite a terceira nota:");
+        nota3 = scan.nextDouble();
         total = (nota1 + nota2 + nota3);
         dividir = (total / 3);
-            if (dividir>6) {
-            System.out.println("Aprovado");
+        if (dividir>6) {
+            System.out.println("Aprovado com a nota: " + dividir);
         }
-            else {
-            System.out.println("Reprovado");
+        else {
+            System.out.println("Reprovado com a nota: " + dividir);
         }
     }
 }
